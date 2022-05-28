15aadeac4d99551870b13972ff67e57c2e022163
tori-getz <torigetz@welcomemedia.ru>
2022-05-28 07:43:10 +0300

    add some bullshit

--- server/dist/logger.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger_service_1 = require("./services/logger.service");
-exports.logger = new logger_service_1.LoggerService('server');
-//# sourceMappingURL=logger.js.map
